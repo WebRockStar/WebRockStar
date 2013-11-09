03e5c96678aff0d53e038cb1c4188f1e4a159209
Akash Chauhan <akash6190@gmail.com>
2013-11-09 05:35:51 -0800

    Adding codemirror

---
+++ public/js/candidate.js
@@ -0,0 +1,8 @@
+/**
+ * @author <akash6190[at]gmail[dot] com>
+ * @desc contains all the application files
+ */
+
+var ce = $('#codeEditor')[0];
+CodeMirror.defaults.lineNumbers = true; 
+var wrsCE = CodeMirror.fromTextArea(ce);
--- routes/candidate.js
+++ routes/candidate.js
@@ -1,7 +1,55 @@
-exports.login = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.login = function(req, res,next) {
+	//verify sessions//
+	
+	if(req.query.id=='test'){
+		next();
+	}else{
+
+		res.render('index', {
+			title : 'Wrong password'
+		});
+
+	}
+	// next();
 };
 
-exports.test = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.test = function(req, res) {
+	// check if the user is loggedin
+	console.log(req.query.id);
+	res.render('problem', {
+		probDesc : 'Some Problem Description',
+		fileList : [{
+			name : 'routes',
+			type : 'folder', //can be folder/file
+			children : [{
+				name : 'index',
+				type : 'file',
+				ext : 'js',
+				contents:'indexjs contents',
+				editable: 'true'
+			}]
+		}, {
+			name : 'views',
+			type : 'folder', //can be folder/file
+			children : [{
+				name : 'index',
+				type : 'file',
+				ext : 'ejs',
+				contents:'indexejs contents',
+				editable: 'false'
+			}]
+		}, {
+			name : 'app',
+			type : 'file', //can be folder/file
+			ext : 'js',
+			contents:'appjs contents',
+			editable: 'true'
+		}, {
+			name : 'package',
+			type : 'file', //can be folder/file
+			ext : 'json',
+			contents:'package.json contents',
+			editable: 'false'
+		}]
+	});
 };
