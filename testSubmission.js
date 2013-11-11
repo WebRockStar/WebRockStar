da53cb79b7adf3005f2d57fc4266b384dac13720
ashriva <ashrivastava@walmartlabs.com>
2013-11-10 18:46:45 -0800

    get problem details

--- testSubmission.js
+++ testSubmission.js
@@ -39,46 +39,50 @@ var app = require('/tmp/'+answerId+'/app.js');
 //this will start the application
 //fetch the testcases for the application
 console.log('will call'+server_name+':'+server_port);
-var resultJSON = {
-	answerId : answerId,
-	problemDifficultyLevel: 'MODERATED',
-	problemId:questionId,
-	problemName:"testData",
-	userEmail:'akash6190@gmail.com'};
 
-var testStatus = new Array();
-testSet.forEach(function(ts,i){
-client.url('http://'+server_name+":"+server_port+ ts.testUrl)
-	//var tcid = v.tcid;
-	try{
-		ts.input.forEach(function(v){
-			for(var x in v){
-				client.setValue(x,v[x]);
-			}
+parse.getObject("Problem",questionId,function(err,res,body,success){
+	var resultJSON = {
+		answerId : answerId,
+		problemDifficultyLevel: body.problemDifficultyLevel,
+		problemId:questionId,
+		problemName:body.problemName,
+		userEmail:'i.abhi27@gmail.com'};
+
+	var testStatus = new Array();
+	testSet.forEach(function(ts,i){
+	client.url('http://'+server_name+":"+server_port+ ts.testUrl)
+		//var tcid = v.tcid;
+		try{
+			ts.input.forEach(function(v){
+				for(var x in v){
+					client.setValue(x,v[x]);
+				}
+			});
+		client.submitForm('form',function(){
+			ts.output.forEach(function(j){
+				for(var y in j){
+					client.getText(y,function(err,val){
+						if(val != j[y]){
+							testStatus.push({tcid:'','status':'Failed'});
+						}
+					});
+
+				}
+			})
 		});
-	client.submitForm('form',function(){
-		ts.output.forEach(function(j){
-			for(var y in j){
-				client.getText(y,function(err,val){
-					if(val != j[y]){
-						testStatus.push({tcid:'','status':'Failed'});
-					}
-				});
-
-			}
-		})
+			resultJSON['testCases'] = testStatus;
+		
+	}catch (e){
+		testStatus.push({'tcid':tcid,'status':'Runtime Error..:\'('})
+			resultJSON['testCases'] = testStatus;
+	//	process.exit(1);
+		}
 	});
-		resultJSON['testCases'] = testStatus;
-	
-}catch (e){
-	testStatus.push({'tcid':tcid,'status':'Runtime Error..:\'('})
-		resultJSON['testCases'] = testStatus;
-//	process.exit(1);
-	}
+	parse.createObject('Solution',resultJSON,function(err,res,body,success){
+		
+		});	
+	//save data
+	//need to store answer as AnswerID
+	//process.exit(0);
+
 });
-parse.createObject('Solution',resultJSON,function(err,res,body,success){
-	
-	});	
-//save data
-//need to store answer as AnswerID
-//process.exit(0);
