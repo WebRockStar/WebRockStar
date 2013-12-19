1621aae016c0422d79731d59d08c7ffb3f2ef050
Akash Chauhan <akash6190@gmail.com>
2013-12-19 16:04:25 +0530

    Add install script phase 1

---
+++ install/git-hooks/removeConfigValues.js
@@ -0,0 +1,29 @@
+// Fetch and store the config values in configValues
+var configValues = require('./config');
+
+/**
+ * removes all the string values from the JSON passed
+ */
+removeValues =function(test) {
+	if(typeof test == 'string'){
+		return '';
+	}
+	for(i in test){
+		if(typeof test[i] == 'object'){
+			test[i] = removeValues(test[i]);
+		}else if(typeof test[i] == 'string'){
+			test[i]='';
+		}
+	}
+	return test;
+};
+
+removeValues(configValues);
+
+/**
+ * Save the newly generated config file to the system.
+ */
+
+
+//This will convert the JSON into a pretty JSON string
+process.stdout.write(JSON.stringify(configValues,null,4));
---
+++ install/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+#TODO: Check if all the packages are installed
+#Copy the git-hooks to ./git/hooks
+cp install/git-hooks/* .git/hooks/ 
